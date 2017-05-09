9fa09d1aaf251a6c37b53053aa7b1b3397085144
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2017-05-09 14:11:51 -0700

    switch objects over to greater store class

--- js/sales.js
+++ js/sales.js
@@ -1,5 +1,64 @@
 'use strict';
 
+//define object class
+
+function Store (location, minCustomers, maxCustomers, salesAverage) {
+    this.location = location;
+    this.minCustomers = minCustomers;
+    this.maxCustomers = maxCustomers;
+    this.salesAverage = salesAverage;
+};
+
+//define universal values
+
+Store.prototype.hoursOpen = 14;
+Store.prototype.salesTotal = 0;
+Store.prototype.hourlySales = [];
+
+//define object class functions
+
+//generate sales estimate
+Store.prototype.salesEstimate = function() {
+    for (var i = 0; i < this.hoursOpen; i++) {
+        var randomSales = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1) * this.salesAverage + this.minCustomers);
+        this.hourlySales.push(randomSales);
+    }
+};
+
+//calculate total daily sales
+Store.prototype.calculateTotal = function() {
+    for (var i = 0; i < this.hourlySales.length; i++) {
+        this.salesTotal += this.hourlySales[i];
+    }
+};
+
+//populate location name
+Store.prototype.populateLocation = function() {
+    var newText = document.createTextNode(this.location);
+    var position = document.getElementsByTagName('lh')[this.objectNumber];
+    position.appendChild(newText);
+};
+
+//populate sales figures
+Store.prototype.populateSales = function() {
+    for (var i = 0; i < this.hourlySales.length; i++) {
+        var newEl = document.createElement('li');
+        var newText = document.createTextNode(this.hourlySales[i]);
+        newEl.appendChild(newText);
+        var position = document.getElementsByTagName('ul')[this.objectNumber];
+        position.appendChild(newEl);
+    }
+};
+
+//create object instances for store locations
+
+var pdxAirportObj = new Store ('PDX Airport', 23, 65, 6.3);
+var pioneerSquareObj = new Store ('Pioneer Square', 3, 24, 1.2);
+var powellsObj = new Store ('Powell\'s', 11, 38, 3.7);
+var stJohnsObj = new Store ('St. John\'s', 20, 38, 2.3);
+var waterfrontObj = new Store ('Waterfront', 2, 16, 4.6);
+
+//////////////////////Outdated Obj Declarations///////////////////////////////////
 var pdxAirportObj = {
     objectNumber: 0,
     location: 'PDX Airport',
